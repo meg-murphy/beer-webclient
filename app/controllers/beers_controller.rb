d64e00f8bc8ac1d25bb23ecfaf29a261f6523a3e
meg-murphy <meghan.murphy.usc@gmail.com>
2016-12-12 21:29:40 -0800

    add edit and update methods

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,20 +1,34 @@
 class BeersController < ApplicationController
 
   def show
-    @beer = Unirest.get("http://localhost:3001/api/v1/beer/#{params[:id]}.json").body
+    @beer = Unirest.get("http://localhost:3001/api/v1/beers/#{params[:id]}.json").body
   end
 
   def index
-    @beers = Unirest.get("http://localhost:3001/api/v1/beer.json").body
+    @beers = Unirest.get("http://localhost:3001/api/v1/beers.json").body
   end
+
+  def create
+    @beer = Unirest.post("http://localhost:3001/api/v1/beers", parameters: {name: params[:name], style: params[:style], hop: params[:hop],
+     yeast: params[:yeast], malts: params[:malts], ibu: params[:ibu], alcohol: params[:alcohol]},
+     headers: {"Accept" => "application/json"}).body
+
+     redirect_to beer_path(@beer["id"])
+   end
+
+
+  def edit
+    @beer = Unirest.get("http://localhost:3001/api/v1/beers/#{params[:id]}.json").body
+  end
+
+
+  def update
+    @beer = Unirest.patch("http://localhost:3001/api/v1/beers", parameters: {id: params[:id], name: params[:name], style: params[:style], hop: params[:hop],
+     yeast: params[:yeast], malts: params[:malts], ibu: params[:ibu], alcohol: params[:alcohol]},
+     headers: {"Accept" => "application/json"}).body
+
+     redirect_to beer_path(@beer["id"])
+  end
+
+
 end
-
-def create
-  
-end
-
-
-
-response = Unirest.post "http://httpbin.org/post",
-                        headers:{ "Accept" => "application/json" },
-                        parameters:{ :age => 23, :foo => "bar" }
